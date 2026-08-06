584f9927c0655368c3e9965852bfc6b4b98630ed
JARVIS <jarvis@billaxe.app>
2026-08-06 15:26:22 -0700

    Remove duplicate: bot-8-engagement.js

--- src/bots/bot-8-engagement.js
+++
@@ -1,8 +0,0 @@
-const { supabaseClient } = require('./db');
-
-async function runBot() {
-  console.log('[bot-8] [INFO] Checking user engagement');
-  return { success: true };
-}
-
-module.exports = { runBot };
