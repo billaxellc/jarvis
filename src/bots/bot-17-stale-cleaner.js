c097512c83adc2471f1b9af0a4e81f65bc0a3686
JARVIS <jarvis@billaxe.app>
2026-08-06 15:26:04 -0700

    Remove duplicate: bot-17-stale-cleaner.js

--- src/bots/bot-17-stale-cleaner.js
+++
@@ -1,8 +0,0 @@
-const { supabaseClient } = require('./db');
-
-async function runBot() {
-  console.log('[bot-17] [INFO] Cleaning stale bills');
-  return { success: true };
-}
-
-module.exports = { runBot };
