bf2989804f651ef97a235fdbab4c1b70fb61bc4f
JARVIS <jarvis@billaxe.app>
2026-08-06 15:25:52 -0700

    Remove duplicate: bot-1-retry-supervisor.js

--- src/bots/bot-1-retry-supervisor.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-const { supabaseClient } = require('./db');
-
-async function runBot() {
-  console.log('[bot-1] [INFO] Test bot running');
-  return { success: true };
-}
-
-module.exports = { runBot };
